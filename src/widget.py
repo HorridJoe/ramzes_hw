7363959dd0476aa53600eb012bfb421810c86ad2
ramzes <romchikpilot@gmail.com>
2024-05-15 19:33:36 +0600

    date function added

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from masks import card_mask
 from masks import account_mask
+from datetime import datetime
 
 def masking_func(str_to_mask: str) -> str:
 
@@ -9,3 +10,9 @@ def masking_func(str_to_mask: str) -> str:
         return account_mask(joined_string[-20:])
     else:
         return card_mask(joined_string[-16:])
+
+
+def return_date(datetime_input: str) -> str:
+     parsed_date = datetime.strptime('2018-07-11T02:26:18.671407',
+                                    '%Y-%m-%dT%H:%M:%S.%f')
+     return datetime.strftime(parsed_date, '%d.%m.%Y')
