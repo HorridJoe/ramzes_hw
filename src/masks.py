ed0cb7549b12891b1b9e9534ccad3e1dbb49debc
ramzes <romchikpilot@gmail.com>
2024-05-15 14:19:36 +0600

    start

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+import os
+
+
+def return_card_mask(card_number: str) -> str:
+    """
+    Возвращает маску карты
+    :param card_number:
+    :return:
+    """
+    return f"{card_number[:4]} {card_number[5:7]}** **** {card_number[12:]}"
+
+
+def return_account_mask(account_number: str) -> str:
+    """
+    Возвращает маску счета
+    :param account_number:
+    :return:
+    """
+    return f"**{account_number[-4:]}"
