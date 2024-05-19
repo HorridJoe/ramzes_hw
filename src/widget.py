4480f728a862c043fb8cf2969d241d974d6e7101
ramzes <romchikpilot@gmail.com>
2024-05-20 02:27:39 +0600

    Добавлены аннотации типов, код приведен к PEP 8

--- src/widget.py
+++ src/widget.py
@@ -4,25 +4,24 @@ from datetime import datetime
 
 
 def masking_func(str_to_mask: str) -> str:
-    '''
+    """
     Возвращает маску карты либо счёта
     :param str_to_mask:
     :return:
-    '''
-    joined_string = str_to_mask.replace(' ', '')
-    if joined_string.startswith('Счет'):
+    """
+    joined_string = str_to_mask.replace(" ", "")
+    if joined_string.startswith("Счет"):
         return account_mask(joined_string[-20:])
     else:
         return card_mask(joined_string[-16:])
 
 
 def return_date(datetime_input: str) -> str:
-    '''
+    """
     Преобразует переданную строку в обьект даты и времени,
     извлекает из него дату в привычном формате
     :param datetime_input:
     :return:
-    '''
-    parsed_date = datetime.strptime(datetime_input,
-                                    '%Y-%m-%dT%H:%M:%S.%f')
-    return datetime.strftime(parsed_date, '%d.%m.%Y')
+    """
+    parsed_date = datetime.strptime(datetime_input, "%Y-%m-%dT%H:%M:%S.%f")
+    return datetime.strftime(parsed_date, "%d.%m.%Y")
