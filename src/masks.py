221d58567e461146c44be2a36f9e93d99e152480
ramzes <romchikpilot@gmail.com>
2024-05-23 12:06:07 +0600

    Добавлены тесты для masks.py

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ def card_mask(card_number: str) -> str:
     :param card_number:
     :return:
     """
-    return f"{card_number[:4]} {card_number[5:7]}** **** {card_number[12:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def account_mask(account_number: str) -> str:
---
+++ tests/test_masks.py
@@ -0,0 +1,18 @@
+import pytest
+from src.masks import card_mask, account_mask
+
+@pytest.mark.parametrize('card_number, expected', [
+    ('2223999900003245', '2223 99** **** 3245'),
+    ('4852805736395734', '4852 80** **** 5734'),
+    ('3842035735485027', '3842 03** **** 5027')
+])
+def test_card_mask(card_number, expected):
+    assert card_mask(card_number) == expected
+
+@pytest.mark.parametrize('acc_number, expected', [
+    ('57305837486958743860', '**3860'),
+    ('37586947586736458305', '**8305'),
+    ('29487593759374659374', '**9374')
+])
+def test_account_mask(acc_number, expected):
+    assert account_mask(acc_number) == expected
