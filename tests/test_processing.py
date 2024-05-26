044a1b193862dacc7585c73b09551c1e33ff6341
ramzes <romchikpilot@gmail.com>
2024-05-26 21:46:52 +0600

    Suggested fixes added

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,22 +1,22 @@
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state_if_executed(ops_list):
-    assert filter_by_state(ops_list) == [
+def test_filter_by_state_if_executed(operations_list):
+    assert filter_by_state(operations_list) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
 
-def test_filter_by_state_if_cancelled(ops_list, state="CANCELED"):
-    assert filter_by_state(ops_list, state) == [
+def test_filter_by_state_if_cancelled(operations_list, state="CANCELED"):
+    assert filter_by_state(operations_list, state) == [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
-def test_sort_by_date_with_reverse(ops_list):
-    assert sort_by_date(ops_list) == [
+def test_sort_by_date_with_reverse(operations_list):
+    assert sort_by_date(operations_list) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -24,8 +24,8 @@ def test_sort_by_date_with_reverse(ops_list):
     ]
 
 
-def test_sort_date_reverse_off(ops_list, reverse=False):
-    assert sort_by_date(ops_list, reverse) == [
+def test_sort_date_reverse_off(operations_list, reverse=False):
+    assert sort_by_date(operations_list, reverse) == [
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
