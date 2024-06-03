8b3a8f865a7fc3531aea0dd333db59795d630609
ramzes <romchikpilot@gmail.com>
2024-06-04 02:29:26 +0600

    Docstrings added, code formatted according to PEP 8, README.md updated

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,30 @@
 from functools import wraps
 
 
-def log(filename=''):
-    def wrapper (func):
+def log(filename=""):
+    '''
+    Декоратор для логирования работы функции в текстовый файл
+    :param filename:
+    :return:
+    '''
+    def wrapper(func):
         @wraps(func)
-        def inner (*args, **kwargs):
+        def inner(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                if filename != '':
-                    with open('mylog.txt', 'w') as file:
-                        file.write(f'{func.__name__} ok')
+                if filename != "":
+                    with open("mylog.txt", "w") as file:
+                        file.write(f"{func.__name__} ok")
                 else:
-                    print(f'{func.__name__} ok')
+                    print(f"{func.__name__} ok")
                 return result
             except Exception as err:
-                if filename != '':
-                    with open('mylog.txt', 'w') as file:
-                        file.write(f'{func.__name__} error: {err}. Inputs: {args}, {kwargs}')
+                if filename != "":
+                    with open("mylog.txt", "w") as file:
+                        file.write(f"{func.__name__} error: {err}. Inputs: {args}, {kwargs}")
                 else:
-                    print(f'{func.__name__} error: {err}. Inputs: {args}, {kwargs}')
+                    print(f"{func.__name__} error: {err}. Inputs: {args}, {kwargs}")
+
         return inner
+
     return wrapper
