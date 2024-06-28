7a9e5c56b37b38a440ea1d1198c0d8599ec91d9c
ramzes <romchikpilot@gmail.com>
2024-06-28 14:49:02 +0600

    Code formatted to PEP 8

--- src/utils.py
+++ src/utils.py
@@ -3,27 +3,28 @@ from src.external_api import ruble_exchange_rate
 
 
 def transactions_readout(transactions_input: str) -> list:
-    '''
+    """
     принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     :param transactions_input:
     :return:
-    '''
+    """
     try:
-        with open(transactions_input, 'r', encoding='utf-8') as file:
+        with open(transactions_input, "r", encoding="utf-8") as file:
             return json.load(file)
     except FileNotFoundError:
         return []
     except ValueError:
         return []
 
+
 def transaction_sum_in_rubles(transaction: dict):
-    '''
+    """
     Принимает на вход транзакцию и возвращает сумму транзакции в рублях
     :param transaction:
     :return:
-    '''
-    currency = transaction["operationAmount"]['currency']["code"]
+    """
+    currency = transaction["operationAmount"]["currency"]["code"]
     amount = transaction["operationAmount"]["amount"]
 
     if currency == "RUB":
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,21 +2,22 @@ from src.utils import transactions_readout
 from src.utils import transaction_sum_in_rubles
 
 
-
 def test_transactions_readout_ok(transactions):
-    input_file = 'test_operations.json'
+    input_file = "test_operations.json"
     result = transactions_readout(input_file)
     expected = transactions
     assert result == expected
 
+
 def test_transactions_readout_empty():
-    input_file = 'test_empty.json'
+    input_file = "test_empty.json"
     result = transactions_readout(input_file)
     expected = []
     assert result == expected
 
+
 def test_transactions_readout_not_found():
-    input_file = 'test_not_found.json'
+    input_file = "test_not_found.json"
     result = transactions_readout(input_file)
     assert result == []
 
@@ -26,42 +27,30 @@ def test_transaction_sum_in_rubles_if_usd(mock_ruble_exchange_rate):
     mock_ruble_exchange_rate.return_value = 75.0
 
     transaction = {
-    "id": 518707726,
-    "state": "EXECUTED",
-    "date": "2018-11-29T07:18:23.941293",
-    "operationAmount": {
-      "amount": "3348.98",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод с карты на карту",
-    "from": "MasterCard 3152479541115065",
-    "to": "Visa Gold 9447344650495960"
-  }
+        "id": 518707726,
+        "state": "EXECUTED",
+        "date": "2018-11-29T07:18:23.941293",
+        "operationAmount": {"amount": "3348.98", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "MasterCard 3152479541115065",
+        "to": "Visa Gold 9447344650495960",
+    }
 
     result = transaction_sum_in_rubles(transaction)
     assert result == 251173.5
 
+
 def test_transaction_sum_in_rubles_if_rub():
     transaction = {
-    "id": 407169720,
-    "state": "EXECUTED",
-    "date": "2018-02-03T14:52:08.093722",
-    "operationAmount": {
-      "amount": "67011.26",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод с карты на карту",
-    "from": "MasterCard 4047671689373225",
-    "to": "Maestro 3806652527413662"
+        "id": 407169720,
+        "state": "EXECUTED",
+        "date": "2018-02-03T14:52:08.093722",
+        "operationAmount": {"amount": "67011.26", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод с карты на карту",
+        "from": "MasterCard 4047671689373225",
+        "to": "Maestro 3806652527413662",
     }
 
     result = transaction_sum_in_rubles(transaction)
 
     assert result == 67011.26
-
