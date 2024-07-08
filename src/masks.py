4341e6b1b44d8959ea69d5710a5ab9b7dc339d91
ramzes <romchikpilot@gmail.com>
2024-07-08 20:25:41 +0600

    Аdded logging for masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,20 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(filename)s - %(levelname)s: %(message)s",
+    filename="logs/masks.log",
+    filemode="w",
+)
+masks_logger = logging.getLogger(__name__)
+
 def card_mask(card_number: str) -> str:
     """
     Возвращает маску карты
     :param card_number:
     :return:
     """
+    masks_logger.info('Возвращена маска введенного номера карты')
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
@@ -13,4 +24,5 @@ def account_mask(account_number: str) -> str:
     :param account_number:
     :return:
     """
+    masks_logger.info('Возвращена маска введенного номера банковского счёта')
     return f"**{account_number[-4:]}"
