99aafd353b248765b923ed15d41fcd4db913ff0c
ramzes <romchikpilot@gmail.com>
2024-07-20 22:52:38 +0600

    Code formatted to PEP 8; README.md updated

--- src/csv_xlsx_readout.py
+++ src/csv_xlsx_readout.py
@@ -1,9 +1,10 @@
 import csv
 import pandas as pd
 
+
 def csv_readout(file_path: str) -> list:
-    '''Принимает на вход путь до .csv файла, возвращает список транзакций'''
-    if file_path.endswith('.csv'):
+    """Принимает на вход путь до .csv файла, возвращает список транзакций"""
+    if file_path.endswith(".csv"):
         with open(file_path) as file:
             reader = csv.DictReader(file, delimiter=";")
             next(reader)
@@ -12,8 +13,9 @@ def csv_readout(file_path: str) -> list:
                 transactions_list.append(row)
         return transactions_list
 
+
 def xlsx_readout(file_path: str) -> list:
-    '''Принимает на вход путь до .xlsx файла, возвращает список транзакций'''
+    """Принимает на вход путь до .xlsx файла, возвращает список транзакций"""
     df = pd.read_excel(file_path)
     transactions_list = df.to_dict(orient="records")
     return transactions_list
--- tests/test_csv_xlsx_readout.py
+++ tests/test_csv_xlsx_readout.py
@@ -13,14 +13,32 @@ def test_xlsx_readout():
 def test_csv_readout(mock_reader):
     """Тестируем чтение файла .csv"""
     test_data = [
-        {'id', 'state', 'date', 'amount', 'currency_name', 'currency_code', 'from', 'to', 'description'},
-        {'650703', 'EXECUTED', '2023-09-05T11:30:32Z', '16210', 'Sol', 'PEN', 'Счет 58803664561298323391',
-         'Счет 39745660563456619397', 'Перевод организации'},
-        {'3598919', 'EXECUTED', '2023-09-05T11:30:32Z', '29740', 'Peso', 'PEN', 'Счет 58803664561298323391',
-         'Счет 39745660563456619397', 'Перевод организации'},
+        {"id", "state", "date", "amount", "currency_name", "currency_code", "from", "to", "description"},
+        {
+            "650703",
+            "EXECUTED",
+            "2023-09-05T11:30:32Z",
+            "16210",
+            "Sol",
+            "PEN",
+            "Счет 58803664561298323391",
+            "Счет 39745660563456619397",
+            "Перевод организации",
+        },
+        {
+            "3598919",
+            "EXECUTED",
+            "2023-09-05T11:30:32Z",
+            "29740",
+            "Peso",
+            "PEN",
+            "Счет 58803664561298323391",
+            "Счет 39745660563456619397",
+            "Перевод организации",
+        },
     ]
     mock_reader.return_value = iter(test_data)
-    result = csv_readout('test.csv')
+    result = csv_readout("test.csv")
     test_data.pop(0)
     assert result == test_data
     mock_reader.assert_called_once()
